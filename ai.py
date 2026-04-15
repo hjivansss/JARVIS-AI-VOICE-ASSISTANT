d6c1f826af1585173e1427bbf7db84c17f02218c
H Jivan Singha <hjivansingha123@gmail.com>
2026-04-15 22:08:36 +0530

    changes in ai.py

--- ai.py
+++ ai.py
@@ -1,6 +1,8 @@
 from google import genai
-
-client = genai.Client(api_key="AIzaSyAUAKiDGJWGRY1BI1cKzdKDV9WK_8MdYQY")
+from dotenv import load_dotenv
+load_dotenv()
+import os
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 models = client.models.list()
 
